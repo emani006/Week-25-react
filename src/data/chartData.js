d5aff83543a472d6644864d3e50f05d2eeae250e
Eleonora Maniecka <emaniecka34@gmail.com>
2022-12-18 14:08:58 +0100

    first update

---
+++ src/data/chartData.js
@@ -0,0 +1,12 @@
+export const data = [
+    ["Task", "Hours per Day"],
+    ["Work", 11],
+    ["Eat", 2],
+    ["Commute", 2],
+    ["Watch TV", 2],
+    ["Sleep", 7],
+];
+
+export const options = {
+    title: "My Daily Activities",
+};
